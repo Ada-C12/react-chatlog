ac9f0f290b0dd5b9532860ecf28d00e18816a33e
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-22 14:30:11 -0800

    added particpants to title

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import ChatLog from './components/ChatLog';
 const App = () => {
   console.log(chatMessages);
 
+  const findParticipants = (messageList) => {
+    const participants = {};
+    messageList.forEach((message) => {
+      const sender = message.sender;
+      participants[sender] = true;
+    });
+    return participants;
+  };
+  const chatParticipants = Object.keys(findParticipants(chatMessages)).join(' and ');
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>{chatParticipants}'s Chat</h1>
       </header>
       <main>
         <ChatLog message_list={chatMessages} />
