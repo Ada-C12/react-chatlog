976e441ae84dd009df0a4c024b32606461dcbaaa
Michaela Morrow <morrow.michaela@gmail.com>
2019-11-22 15:53:30 -0800

    Updated title to change dynamically with names

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+  <h1>Chat Between {chatMessages[0].sender} and {chatMessages[1].sender}</h1>
       </header>
       <main>
         {/* <ChatEntry sender='Vladimir' body='why are you arguing with me' time='2018-05-29T22:49:06+00:00' /> */}
