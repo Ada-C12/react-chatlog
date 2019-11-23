ee366708267fa50b8e9b931f30de1effba11129e
Diana <dianah46@gmail.com>
2019-11-22 23:52:12 -0800

    installed propTypes

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './ChatEntry.css';
+import Timestamp from './Timestamp';
+import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
   return (
-    
+    <div className="chat-entry">
+      <p className="entry-name"> {props.sender}</p>
+      <p className="entry-bubble"> {props.body}</p>
+      <p className="entry-time"> <Timestamp time={props.timeStamp}/></p>
+    </div>
 
-  )
+  );
 }
 
+ChatEntry.propTypes = {
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+}
 export default ChatEntry;
