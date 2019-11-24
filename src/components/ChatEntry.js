111f3fd1d1c34665d66e8b235651908d883b8462
Eve Le <nguyenthao.lee@gmail.com>
2019-11-24 01:52:13 -0800

    Added styling for ChatEntry.js

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -4,16 +4,18 @@ import './ChatEntry.css'
 
 const ChatEntry = () => {
     const entry = {
-        "sender":"Vladimir",
-        "body":"why are you arguing with me",
-        "timeStamp":"2018-05-29T22:49:06+00:00"
-      };
+        "sender": "Vladimir",
+        "body": "why are you arguing with me",
+        "timeStamp": "2018-05-29T22:49:06+00:00"
+    };
 
     return (
-        <section>
-            <span>{entry.sender}</span>
-            <p>{entry.body}</p>
-            <Timestamp time={entry.timeStamp}/>
+        <section className="chat-entry local">
+            <strong className="entry-name">{entry.sender}</strong>
+            <section className="entry-bubble">
+                <p>{entry.body}</p>
+                <section className="entry-time"><Timestamp time={entry.timeStamp} /></section>
+            </section>
         </section>
     );
 };
