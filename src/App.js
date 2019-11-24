49d40b692312bdf16ac5ef8945c9070fb920153c
Nicky Choi <nickyjinchoi@gmail.com>
2019-11-23 20:12:56 -0800

    Edited h1 text and added classNames

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
-  console.log(chatMessages);
 
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1 className="App-title">Chat between {chatMessages[0].sender} and {chatMessages[1].sender} </h1>
       </header>
-      <main>
-
+      <main className="App-Main">
+        <ChatLog messages={chatMessages}/>
       </main>
     </div>
   );
