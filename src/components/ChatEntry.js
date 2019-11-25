ed10ce5a25ffc9920cef1f24879227422399e484
geli-gel <angele.zam@gmail.com>
2019-11-24 21:01:56 -0800

    ChatLog creates array of chatEntries from chatMessages json and returns them in a div

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './ChatEntry.css';
 import Timestamp from './Timestamp';
 
-const ChatEntry = ({sender, body, timestamp, location}) => {
-  // const alignmentClass = location == true ? "chat-entry local" : "chat-entry remote";
+const ChatEntry = ({sender, body, timestamp, local}) => {
+  const alignmentClass = local === true ? "chat-entry local" : "chat-entry remote";
   
   return (
-    <div className="chat-entry local remote">
+    <div className={alignmentClass}>
       <div className="entry-name">
         {sender}
       </div>
       <div className="entry-bubble">
         {body}
-        <div className="entry-time"><Timestamp className="entry-time"  time={timestamp} /></div>
+        <div className="entry-time">
+          <Timestamp  time={timestamp} />
+        </div>
       </div>
     </div>
   )
