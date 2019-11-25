3124b375bf77640d524173cdf2a26aa4cf1a281f
gracexinran <gracexinran@gmail.com>
2019-11-24 22:11:03 -0800

    Created HeaderTitle component and implemented it to App header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
+import HeaderTitle from './components/HeaderTitle';
 
 const App = () => {
   console.log(chatMessages);
@@ -9,7 +10,9 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>
+          <HeaderTitle allChats={chatMessages}/>
+        </h1>
       </header>
       <main>
         <ChatLog allChats={chatMessages}/>
