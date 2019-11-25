390b070a48a1a7ffb7bd8fb57f3a5a618542e1a8
Brianna Kemp <kemp.bri@gmail.com>
2019-11-24 22:28:31 -0800

    Added remote and local classes for chat entry

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -3,13 +3,20 @@ import PropTypes from 'prop-types';
 import Timestamp from './Timestamp';
 import './ChatEntry.css'
 
+const sender = (name) => {
+  if (name === "Vladimir") {
+    return "local"
+  } else {
+    return "remote"
+  }
+}
 
 const chatEntry = (props) => {
   
 
   return (
     // <section className={`chat-entry.${sender(props.sender)}`}>
-    <section className="chat-entry">
+    <section className={`chat-entry ${sender(props.sender)}`}>
       <h3 className="entry-name">{props.sender}</h3>
       <section className="entry-bubble">
         <p>{props.body}</p>
