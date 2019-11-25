670cdd4936b3b1ad4a8f55652295b1b427d50873
Eve Le <nguyenthao.lee@gmail.com>
2019-11-25 00:37:09 -0800

    Modified app header to use dynamic sender data

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog.js';
 
 const App = () => {
-  console.log(chatMessages);
+  let senders = new Set();
+  for (let i = 0; i < chatMessages.length; i++) {
+    senders.add(chatMessages[i].sender);
+  }
+
+  let recipients = "";
+  let counter = 1;
+  senders.forEach(sender => {
+    if (counter === 1) {
+      recipients += ` ${sender}`;
+    } else if (counter < senders.size) {
+      recipients += `, ${sender}`;
+    } else if (counter === senders.size) {
+      recipients += ` and ${sender}`;
+    }
+    counter += 1;
+  });
 
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between{recipients} </h1>
       </header>
       <main>
         <ChatLog log={chatMessages}></ChatLog>
